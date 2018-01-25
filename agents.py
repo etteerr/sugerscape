532ff088632712f54404d2882dc60dd8d9afda81
etteerr <e.j.diepgrond@gmail.com>
2018-01-25 11:05:56 +0100

    Updated Evo agent, now works

--- agents.py
+++ agents.py
@@ -106,7 +106,7 @@ class SugerAgent(Agent):
         The direction utility calculator
     '''
     def movUtility(self, highestResource, distance, resource):
-        return (max_val)/(0.001 * self.wealth[resource] + 0.1 * distance);
+        return (highestResource)/(0.001 * self.wealth[resource] + 0.1 * distance);
                     
     
     def choose_direction(self):
@@ -222,10 +222,52 @@ class SugerAgent(Agent):
         
         
 class EvolutionAgent(SugerAgent):
-    self.genome = None;
-    self.genome_if = ['vision', ];
+    genome = None;
+    genome_if = ['vision', 'alpha', 'beta', 'gamma', 'needs1', 'needs2'];
+    needs_genome_offset = 4;
+    
     def __init__(self):
+            self.genome = np.random.rand(len(self.genome_if)) * 10 - 5;
+            
+    def init(self):
+        assert(self.model is not None), "Model is None, was agent added to model?";
+        assert(self.grid is not None), "Grid is None, Agent was not placed in a grid. This agent requires a grid";
         
+        # Die if we have invalid genome
+            #Vision is smaller than 0
+            #or a need is smaller than 0
+        if \
+        np.round(self.genome[0]) <= 0 or \
+        self.genome[4] < 0 or \
+        self.genome[5] < 0:
+            self.die();
+        
+        # Initialize wealth
+        self.wealth = dict();
+        for r_name in self.grid.resources:
+            self.wealth[r_name] = np.random.randint(self.wealth_low, self.wealth_high);
+            
+        # age
+        self.age = 0;
+        
+        # Metabolism
+        self.metabolism = 0.5 * self.genome[0] + 1; #1/2 vision is metabolism (7 is thus evolutionary max vis)
+        
+        # What resources are needed to fill the metabolism
+        self.needs = dict();
+        i=0;
+        for r_name in self.grid.resources:
+            self.needs[r_name] = self.genome[self.needs_genome_offset + i];
+            i+=1;
+        normalize_dict(self.needs); #make vectorlength 1
+        
+        #harvest efficiency
+        self.harvest_eff = dict();
+        for r_name in self.grid.resources:
+            self.harvest_eff[r_name] = 1.0;
+            
+        # Vision
+        self.vision = int(np.round(self.genome[0]));
     '''
         Define how the agent moves
     '''
@@ -243,7 +285,6 @@ class EvolutionAgent(SugerAgent):
         #harvest
         self.grid.harvest(self, self.harvest_eff);
         #Metabolise
-        self.metabolism = (4*self.vision/);
         for n in self.wealth:
             self.wealth[n] -= self.metabolism * self.needs[n];
         
@@ -253,24 +294,42 @@ class EvolutionAgent(SugerAgent):
                 return; #Cant die twice
             
         # Age
-        self.age += 1;
-        
-        # reproduce
-        '''
-        accumulated_wealth = sum(self.wealth.values());
-        if accumulated_wealth > 10:
-            offspring = SugerAgent();
-            canidate_places= self.model.grid.get_move_canidates(self);
-            if len(canidate_places) > 0:
-                self.model.add_agent(offspring, position=random.choice(canidate_places));
-                for key in self.wealth:
-                    self.wealth[key] -= 5;
-         '''   
+        self.age += 1; #Kept as a age counter for fitness
         
             
     def die(self):
         self.model.remove_agent(self);
         self.model.dead.append(self);
         
+    '''
+        The direction utility calculator
+    '''
+    def movUtility(self, highestResource, distance, resource):
+        return self.genome[1] * highestResource + self.genome[2] * self.wealth[resource] + self.genome[3] * distance;
+        
     def fitness(self):
         pass
+    
+    '''
+        Describe the agent as a string
+    '''
+    def __str__(self):
+        description = [];
+        description.append("Agent %i" % self.id);
+        description.append("\tMetabolism: %f" % (self.metabolism));
+        for k,v in self.needs.items():
+            description.append("\t\tneed %f%% %s" %(v*100.0,k));
+        description.append("\tAge: %i" % (self.age));
+        description.append("\tVision (%f): %i" % (self.genome[0], self.vision));
+        description.append("\twealth (%f-%f):" %(self.wealth_low, self.wealth_high));
+        for name, wealth in self.wealth.items():
+            description.append("\t\t%s: %f" % (name,wealth));
+            
+        description.append("\tGenome:");
+        for i in range(len(self.genome)):
+            description.append("\t\t%s: %f" %(self.genome_if[i], self.genome[i]));
+        
+        fin_str = "";
+        for i in description:
+            fin_str += i + "\n";
+        return fin_str;
